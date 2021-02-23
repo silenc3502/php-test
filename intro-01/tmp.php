9301d2f36ca5b1b97ad15b0e725c2459626b3d6b
silenc3502 <gcccompil3r@gmail.com>
2021-02-24 03:07:11 +0900

    PHP Test

---
+++ intro-01/tmp.php
@@ -0,0 +1,8 @@
+<?php
+$number = 12345 * 67890;
+echo substr($number, 3, 1);
+echo "<br>";
+echo substr($number, 3, 3);
+echo "<br>";
+echo $number;
+?>
---
+++ intro-01/variable.php
@@ -0,0 +1,13 @@
+<?php
+$mycounter = 1;
+$mystring  = "Hello";
+$myarray   = array("One", "Two", "Three");
+
+echo $mycounter;
+echo "<br>";
+echo $mystring;
+echo "<br>";
+echo $myarray[0];
+echo $myarray[1];
+echo $myarray[2];
+?>
